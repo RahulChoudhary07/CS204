abc8a9ec376bf9fa8700e8a70dfa939f75ebc51f
RahulYoYo07 <53635023+RahulYoYo07@users.noreply.github.com>
2019-09-09 15:25:42 +0530

    Add files via upload

---
+++ lab_7/greatest.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+int comparison(string X, string Y) 
+{ 
+    
+    string XY = X.append(Y); 
+  
+    
+    string YX = Y.append(X); 
+  
+    
+    return XY.compare(YX) > 0 ? 1: 0; 
+} 
+  
+
+
+
+int main() 
+{ 
+    
+    long long int  T;
+    cin>>T;
+    while(T--)
+    {
+       long long int  n;
+       cin>>n;
+       string s[n];
+       for(long long int  i=0;i<n;i++)
+       {
+           cin>>s[i];
+       }
+       sort(s,s+n,comparison);
+       for (long long int  i=0; i < n ; i++ ) 
+       {
+           cout << s[i]; 
+       }
+       cout<<endl;
+    }
+
+   return 0; 
+}
+
+
+
+
+
